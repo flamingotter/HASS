8830b9babf9f3bd6d2be858bc9f7f301d838bc50
Josh <38353250+flamingotter@users.noreply.github.com>
2018-09-28 14:49:24 +0200

    Update dummy_secrets.sh

--- bin/dummy_secrets.sh
+++ bin/dummy_secrets.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 echo "http_password: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "home_lat: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "home_long: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "work_lat: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "work_long: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "ifttt_key: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "google_secret: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "mqtt_password: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "mqtt_username: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
-echo "ssl_key: ssl_key.pem" >> $TRAVIS_BUILD_DIR/secrets.yaml
-echo "ssl_cert: ssl_cert.pem" >> $TRAVIS_BUILD_DIR/secrets.yaml
-echo "db_url: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
-touch "ssl_key.pem"
-touch "ssl_cert.pem"
+echo "kodi_living_room: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "unifi_username: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "unifi_password: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "influx_database: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "influx_username: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "influx_password: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "darksky_api_key: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
