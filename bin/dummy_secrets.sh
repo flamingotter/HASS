1e71a82e7c079c6d7437a4908f72eed4b73e9e7f
Josh <38353250+flamingotter@users.noreply.github.com>
2018-09-28 14:27:45 +0200

    Update dummy_secrets.sh

--- bin/dummy_secrets.sh
+++ bin/dummy_secrets.sh
@@ -2,7 +2,7 @@
 
 echo "http_password: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "google_secret: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
-echo "mqtt_pass: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "mqtt_password: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "mqtt_username: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "ssl_key: ssl_key.pem" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "ssl_cert: ssl_cert.pem" >> $TRAVIS_BUILD_DIR/secrets.yaml
