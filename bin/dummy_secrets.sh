ac9f7063efdd853c5f03fa7224f959579085773d
Josh <38353250+flamingotter@users.noreply.github.com>
2018-09-28 16:26:02 +0200

    Update dummy_secrets.sh

--- bin/dummy_secrets.sh
+++ bin/dummy_secrets.sh
@@ -6,7 +6,6 @@ echo "home_long: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "work_lat: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "work_long: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "ifttt_key: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
-echo "google_secret: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "mqtt_password: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "mqtt_username: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "kodi_living_room: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
